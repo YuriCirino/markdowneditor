ea59df7f9c5b18d0760caf7cc76ef34cf7412bdb
Yuri Cirino <yuricirino@outlook.com>
2022-12-28 02:56:41 -0300

    Adding italic and blockquote tags

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-//separate the text in lines
-//analize in the line in:
-//Heading tags
-//Bold tags
+// Using regex-replace to:
+    //bold tag,
+    //italic tag
+    //link tag
+    //image tag
+//Then split by \n
+//Indetify the Heading tags
+    //When analizyng the heading tags, is important take care on blank lines with line break
+    //because it results in <p></p> 
 //Anchor tags
 const regexs = {
     headings: {
@@ -14,7 +19,19 @@ const regexs = {
     },
 
     'link' : /[\[]([\w+|\s+]+)[\]][(]([\S+]+)[)]/gm,
-    'image':/![\[]([\w+|\s+]+)[\]][(]([\S+]+)[)]/gm
+    'image':/![\[]([\w+|\s+]+)[\]][(]([\S+]+)[)]/gm,
+    blockquote: /(^>\s)/gm
+
+}
+const stylesByTag = {
+    h1: " font-bold text-4xl mb-3",
+    h2: " font-bold text-3xl mb-2.5",
+    h3: " font-bold text-2xl mb-2",
+    h4: " font-bold text-xl mb-1.5",
+    h5: " font-bold text-lg mb-1.5",
+    h6: " font-bold text-base mb-1.5",
+    p: " my-1",
+    blockquote: " px-6 py-2 bg-gray-100 border-l-4 border-gray-400 "
 }
 
 function CreateNewElement(tag, text) {
@@ -29,34 +46,13 @@ let result = document.getElementById("result")
 let timer
 let lines
 
-// function processChange() {
-//   let text
-//   let tagToCreate = 'div'
-//   clearTimeout(timer)
-//   timer = setTimeout(() => {
-//     result.innerHTML = ''
-//     text = main.value
-//     lines = text.split('\n')
-
-//     lines.forEach((line) => {
-//       Object.entries(regexs).forEach(arr => {
-//         let [tag, expression] = arr
-//         if (line.match(expression)) {
-//           tagToCreate = tag
-//           line = line.replace(expression, '$2')
-//         }
-//       })
-
-//       result.appendChild(CreateNewElement(tagToCreate, line))
-//     })
-//   }, 300)
-// }
-
 function processChange2() {
-    let newInnerHtml = ""
+    let newInnerHtml
     clearTimeout(timer)
     timer = setTimeout(() => {
+        newInnerHtml = ""
         let plainText = main.innerText
+        //spliting by "**" to add strong tag
         let splited = plainText.split("**")
         splited.forEach((part, index) => {
             if (index % 2 == 0) {
@@ -65,28 +61,50 @@ function processChange2() {
                 newInnerHtml += `<strong>${part}</strong>`
             }
         })
+        
+        plainText = newInnerHtml
+        newInnerHtml = "" // reseting newInnerHtml
+        //spliting by "__" to add i tag
+        splited = plainText.split("__")
+        splited.forEach((part,index) => {
+            if(index % 2 == 0) {
+                newInnerHtml += part
+            }else{
+                newInnerHtml += `<i>${part}</i>`
+            }
+        })
+        //
         let selectedTag = ""
         result.innerHTML = newInnerHtml
         plainText = newInnerHtml
         newInnerHtml = ""
+        //spliting text by line breaks
         splited = plainText.split("\n")
+        console.log('splited by break line->',splited)
         splited.forEach((line) => {
-            selectedTag = "p"
+            //going throug lines  to identify headings 
+            selectedTag = "p" // by default, if no headings or blockquotes were found, put the line in paragraph tag
 
-            Object.entries(regexs.headings).forEach((arr) => {
+            Object.entries(regexs.headings).forEach((arr) => { // identifying headings, going through heading regexs objetc
                 let [tag, expression] = arr
                 if (line.match(expression)) {
                     line = line.replace(expression, "")
                     selectedTag = tag
                 }
             })
-            newInnerHtml += `<${selectedTag}>${line}</${selectedTag}>`
+            // now identifying blockquote in the line
+            if(line.match(regexs.blockquote)) {
+                selectedTag = "blockquote"
+                line = line.replaceAll(regexs.blockquote, "")
+            }
+            newInnerHtml += `<${selectedTag} class="${stylesByTag[selectedTag]}">${line}</${selectedTag}>`
         })
-        console.log(newInnerHtml)
         newInnerHtml = newInnerHtml.replaceAll(regexs.image,'<img alt="$1" src="$2">')
-        newInnerHtml = newInnerHtml.replaceAll(regexs.link, '<a href="$2">$1</a>')
+        newInnerHtml = newInnerHtml.replaceAll(regexs.link, '<a href="$2" class="text-sky-500 p-1 bg-gray-100 rounded">$1</a>')
         result.innerHTML = newInnerHtml
     }, 300)
 }
 
 main.addEventListener("input", processChange2)
+main.addEventListener("focus", processChange2)
+
